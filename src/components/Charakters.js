c022fccbd62bf2ea056f6cdddadbe853ee699aea
NataliaGatarek <nova.nathalia@gmail.com>
2021-05-06 12:54:20 +0200

    modals

--- src/components/Charakters.js
+++ src/components/Charakters.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import "./Charakters.css"
 import Button from 'react-bootstrap/Button'
 import { Modal } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Charakters(props) {
 
@@ -19,24 +20,27 @@ function Charakters(props) {
             </div>
             <div className="flip-card-back display">
                     <strong>{props.character.name}</strong>
-                    <Button variant="outline-primary" onClick={handleShow}>More</Button>{' '}
+                    <Button variant="outline-danger" onClick={handleShow}>More</Button>{' '}
                     </div>
                      <Modal Modal
                           show={show}
                           onHide={handleClose}
                           backdrop="static"
-                    keyboard={false}>
-                          <Modal.Header>
-                          <Modal.Title className="modal-title">{props.character.name}</Modal.Title>
+                    keyboard={false}
+                >
+                          <Modal.Header className="disp">
+                          <Modal.Title><strong>{props.character.name}</strong></Modal.Title>
                           </Modal.Header>
-                          <Modal.Body className="display">
-                        <img className="modal-img" src={props.character.image} />
+                    <Modal.Body className="displaying" >
+                        <div className="display">
+                        <img src={props.character.image} />
                         <strong> {props.character.species} </strong>  
-                        <strong> {props.character.gender}</strong>   
-                         <strong>{props.character.status}</strong>  
+                        <strong> {props.character.gender} </strong>   
+                        <strong> {props.character.status} </strong>
+                        </div>
                           </Modal.Body>
-                          <Modal.Footer className="display">
-                          <Button variant="secondary" onClick={handleClose}>Close</Button>
+                          <Modal.Footer className="disp">
+                          <Button variant="outline-danger" onClick={handleClose}>Close</Button>
                         </Modal.Footer>
                         </Modal>
                      
